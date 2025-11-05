543109e2469783e62b5c9592a63226dc353a6b38
João Krol <joaokroltrabalho@gmail.com>
2025-11-05 13:59:50 -0300

    n

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
-// const cors = require("cors");
+const cors = require("cors");
 // const https = require("https");
 // var fs = require("fs");
 
@@ -8,7 +8,7 @@ const app = express();
 const port = 4000;
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 
 const connect = () => {
   const db = mysql.createConnection({
